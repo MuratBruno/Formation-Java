618372fbeca09cb60833b99993782c8887b7457c
Samy <35836274+xhackax47@users.noreply.github.com>
2018-04-25 10:07:55 +0200

    Add files via upload

---
+++ TestEjb/src/servlet/TestHello2.java
@@ -0,0 +1,49 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import ejb.HelloSingleton;
+
+/**
+ * Servlet implementation class TestHello1
+ */
+@WebServlet("/TestHello2")
+public class TestHello2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+	@EJB
+	public HelloSingleton hello;
+	
+    public TestHello2() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append("Réponse du singleton numero 2 : "+ hello.getHello());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
