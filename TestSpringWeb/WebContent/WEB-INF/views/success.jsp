c19d1d6a4f36114718bab59ea04cccf83a00d042
Samy <35836274+xhackax47@users.noreply.github.com>
2018-05-14 16:38:55 +0200

    Add files via upload

---
+++ TestSpringWeb/WebContent/WEB-INF/views/success.jsp
@@ -0,0 +1,6 @@
+ <html>
+<head><title>Login Success</title></head>
+<body>
+   Login Message : ${message}
+</body>
+</html>
---
+++ TestSpringWeb/src/hello/SecurityInitializer.java
@@ -0,0 +1,7 @@
+package hello;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
